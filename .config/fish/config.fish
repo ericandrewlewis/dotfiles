e274a9e9fcf000fa08239aca68f503ca2bab1d35
Eric Lewis <eric.andrew.lewis@gmail.com>
2017-12-18 16:14:25 -0500

    Make some shell vars available in subprocesses

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,12 +4,12 @@ status --is-interactive; and source (rbenv init -|psub)
 # Some environment variables
 bass source ~/.cc_env
 
-set NVM_DIR "$HOME/.nvm"
+set --export NVM_DIR "$HOME/.nvm"
 bass source (brew --prefix nvm)/nvm.sh
 
 # GOPATH
-set GOPATH $HOME/go
-set GOROOT /usr/local/opt/go/libexec
+set --export GOPATH $HOME/go
+set --export GOROOT /usr/local/opt/go/libexec
 set PATH $PATH $GOPATH/bin
 set PATH $PATH $GOROOT/bin
 set PATH /usr/local/opt/elasticsearch@1.7/bin $PATH
