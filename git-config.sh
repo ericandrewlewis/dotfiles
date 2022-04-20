93b270135ae5dcb2c51179378a2f271a6750b2a6
Eric Lewis <eric.andrew.lewis@gmail.com>
2022-04-20 05:00:32 -0400

    Add git-config.sh

---
+++ git-config.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git config --global user.name "Eric Lewis"
+git config --global user.email eric.andrew.lewis@gmail.com
+# Use VSCode as the default editor for git
+git config --global core.editor "code --wait"
+# Set `main` as the default branch name
+git config --global init.defaultBranch main
